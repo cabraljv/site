14a25056c232a7d14cb51b578b3c3f239731f640
João Victor <joaovictorcabralsteam@gmail.com>
2019-06-20 22:56:05 -0300

    Updates

---
+++ precache-manifest.e13bf0418018ce95fb31069a2cc410df.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01f4c1c606e898f57f1094fa5a10fe4",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "277dcd44b47a3b65c16a",
+    "url": "/site/static/css/main.94c30330.chunk.css"
+  },
+  {
+    "revision": "d393c19acf0e347bd2d7",
+    "url": "/site/static/js/2.b957372d.chunk.js"
+  },
+  {
+    "revision": "277dcd44b47a3b65c16a",
+    "url": "/site/static/js/main.f40eb2d9.chunk.js"
+  },
+  {
+    "revision": "14c83684653b1b55fe6d",
+    "url": "/site/static/js/runtime~main.de27f395.js"
+  },
+  {
+    "revision": "99d7bbd2ebfb01102b8b33a4ed90d3de",
+    "url": "/site/static/media/1.99d7bbd2.jpg"
+  },
+  {
+    "revision": "439d78e88adfd8b8dec0065f0a3d6347",
+    "url": "/site/static/media/2.439d78e8.jpg"
+  },
+  {
+    "revision": "663fcaf7ef720bd4ca950a8438e57586",
+    "url": "/site/static/media/3.663fcaf7.jpg"
+  },
+  {
+    "revision": "0535c23e13109694800a998ecf0dd635",
+    "url": "/site/static/media/bandeira.0535c23e.svg"
+  },
+  {
+    "revision": "cba8043a01489084898f78ea25b42c5a",
+    "url": "/site/static/media/bolafut.cba8043a.svg"
+  },
+  {
+    "revision": "2b2d0501bf871c36fc6f5652ed46edea",
+    "url": "/site/static/media/closeicon.2b2d0501.svg"
+  },
+  {
+    "revision": "5e36153437a09422a94c7eb6d4097564",
+    "url": "/site/static/media/gincana.5e361534.svg"
+  },
+  {
+    "revision": "f9265dce55a0ed431baaa816c5d526ed",
+    "url": "/site/static/media/handball.f9265dce.svg"
+  },
+  {
+    "revision": "802f2582662486e2f7cda4544c6d9804",
+    "url": "/site/static/media/logoJogos.802f2582.svg"
+  },
+  {
+    "revision": "fe18e258c57af259099f25eb9fda5cb6",
+    "url": "/site/static/media/logofundo.fe18e258.svg"
+  },
+  {
+    "revision": "45503df6a9982f5e530d7a1087ff76e3",
+    "url": "/site/static/media/peteca.45503df6.svg"
+  },
+  {
+    "revision": "4e1e1306ca0a3a355fa968331c3ae509",
+    "url": "/site/static/media/queimada.4e1e1306.svg"
+  },
+  {
+    "revision": "a6a6ce6edd62e1a7577df28af257da49",
+    "url": "/site/static/media/vermelho.a6a6ce6e.svg"
+  },
+  {
+    "revision": "19d9ddf1abf67354c0ddf33b2644b4d7",
+    "url": "/site/static/media/volei.19d9ddf1.svg"
+  },
+  {
+    "revision": "269a242c1ddec6e1fa6fa7cc48bfbda0",
+    "url": "/site/static/media/xadrez.269a242c.svg"
+  }
+]);
